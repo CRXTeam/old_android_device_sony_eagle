e410055bdae610f60d8a673e8aa67190eadc88e1
cr3pt <cr3pt@o2.pl>
2014-10-12 21:18:26 +0200

    Move to kitkat and CPA

--- full_eagle.mk
+++ full_eagle.mk
@@ -16,9 +16,6 @@
 TARGET_SCREEN_HEIGHT := 960
 TARGET_SCREEN_WIDTH := 540
 
-# Torch
-PRODUCT_PACKAGES := \
-    Torch
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
